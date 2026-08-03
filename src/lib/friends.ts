3e95c0e5b284ef7533fce8efe3ae02707a03d963
lyimoexiao <lyimoexiao@outlook.com>
2026-08-03 13:12:10 +0800

    feat: use circular avatar as favicon and add new friends

--- src/lib/friends.ts
+++ src/lib/friends.ts
@@ -36,4 +36,22 @@ export const friends: Friend[] = [
     date: '2023-04-04',
     feed: '',
   },
+  {
+    author: '纸鹿摸鱼处',
+    desc: '纸鹿至麓不知路，支炉制露不止漉',
+    link: 'https://blog.zhilu.site',
+    icon: 'https://www.zhilu.site/api/avatar.png',
+    avatar: 'https://www.zhilu.site/api/avatar.png',
+    date: '2025-12-15',
+    feed: 'https://blog.zhilu.site/atom.xml',
+  },
+  {
+    author: 'ChuYao',
+    desc: '不知道干什么就只有摆烂了',
+    link: 'https://blog.yaooa.cn',
+    icon: 'https://blog.adokiu.com/avatar.png',
+    avatar: 'https://blog.adokiu.com/avatar.png',
+    date: '2026-01-25',
+    feed: 'https://blog.yaooa.cn/rss.xml',
+  },
 ]
